3593eb37ce0dc2254014fb4a21bc437c169d0543
Alex Bogovich <bogovich.alexander@gmail.com>
2019-04-17 16:31:46 +0700

    create sample

--- src/main/kotlin/io/github/alexbogovich/springintro/SpringIntroApplication.kt
+++ src/main/kotlin/io/github/alexbogovich/springintro/SpringIntroApplication.kt
@@ -2,6 +2,7 @@ package io.github.alexbogovich.springintro
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.*
 
 @SpringBootApplication
 class SpringIntroApplication
@@ -9,3 +10,22 @@ class SpringIntroApplication
 fun main(args: Array<String>) {
 	runApplication<SpringIntroApplication>(*args)
 }
+
+@RestController
+//@Controller + @ResponseBody
+class SimpleController {
+
+	@GetMapping("/hello")
+	fun helloAll() = "hello all!"
+	@GetMapping("/hello/{name}")
+	fun helloAll(@PathVariable name: String) = "hello $name!"
+
+	@GetMapping("/query")
+	fun query(@RequestParam("param") name: Int) = "param = $name!"
+
+	@PostMapping("/post")
+	fun post(@RequestBody body: SimpleRequest) = "body = $body!"
+
+}
+
+data class SimpleRequest(val name: String)
